131c197350151ce52701024c688fc1d82b837ba2
rxsweet <105535100+rxsweet@users.noreply.github.com>
2024-09-12 11:55:19 +0800

    Update run.py

--- run.py
+++ run.py
@@ -27,8 +27,8 @@ def ssh_multiple_connections(hosts_info, command):
             
             #登录后添加自己保活任务
             #https://blog.csdn.net/weixin_42366275/article/details/111628923
-            #ssh.exec_command(del_cron)
-            #print('定时任务清除成功! \n')
+            ssh.exec_command(del_cron)
+            print('定时任务清除成功! \n')
             ssh.exec_command(vmess_ilive)
             print('添加定时任务成功! \n')
             
