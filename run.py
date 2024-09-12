e540016aafd635a90cd21426ca9620985bed74ae
rxsweet <105535100+rxsweet@users.noreply.github.com>
2024-09-12 11:45:38 +0800

    Update run.py

--- run.py
+++ run.py
@@ -19,6 +19,11 @@ def ssh_multiple_connections(hosts_info, command):
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(hostname=hostname, port=22, username=username, password=password)
+            stdin, stdout, stderr = ssh.exec_command(command)
+            user = stdout.read().decode().strip()
+            users.append(user)
+            hostnames.append(hostname)
+            print('\n已登录成功了! \n')
             #登录后添加自己保活任务
             #https://blog.csdn.net/weixin_42366275/article/details/111628923
             ssh.exec_command(del_cron)
@@ -26,11 +31,6 @@ def ssh_multiple_connections(hosts_info, command):
             print('\n 输入的信息：' + stdin.readlines())
             print('\n 输出的信息：' + stdout.readlines())
             print('\n 错误的信息：' + stderr.readlines())
-            
-            stdin, stdout, stderr = ssh.exec_command(command)
-            user = stdout.read().decode().strip()
-            users.append(user)
-            hostnames.append(hostname)
             ssh.close()
         except Exception as e:
             print(f"错误提示：   用户：{username}，连接 {hostname} 时出错: {str(e)}")
